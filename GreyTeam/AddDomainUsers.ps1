7418910cc9a182dbe8a6bc988ba44751b884bc4e
Cullen <cxr5971@rit.edu>
2020-10-12 01:45:22 -0400

    Added domain user script

---
+++ GreyTeam/AddDomainUsers.ps1
@@ -0,0 +1,9 @@
+New-ADUser -Name "Bruce Herring" -SamAccountName "bruceherring" -AccountPassword (ConvertTo-SecureString -AsPlainText "PleaseChange123!") -Enabled $True
+New-ADUser -Name "Daryl Johnson" -SamAccountName "daryljohnson" -AccountPassword (ConvertTo-SecureString -AsPlainText "PleaseChange123!") -Enabled $True
+New-ADUser -Name "Bill Stackpole" -SamAccountName "billstackpole" -AccountPassword (ConvertTo-SecureString -AsPlainText "PleaseChange123!") -Enabled $True
+New-ADUser -Name "Garret Arcoraci" -SamAccountName "garretarcoraci" -AccountPassword (ConvertTo-SecureString -AsPlainText "PleaseChange123!") -Enabled $True
+New-ADUser -Name "Rob Olson" -SamAccountName "robolson" -AccountPassword (ConvertTo-SecureString -AsPlainText "PleaseChange123!") -Enabled $True
+New-ADUser -Name "Justin Pelletier" -SamAccountName "justinpelletier" -AccountPassword (ConvertTo-SecureString -AsPlainText "PleaseChange123!") -Enabled $True
+New-ADUser -Name "Chaim Sanders" -SamAccountName "chaimsanders" -AccountPassword (ConvertTo-SecureString -AsPlainText "PleaseChange123!") -Enabled $True
+New-ADUser -Name "Bo Yuan" -SamAccountName "Bo Yuan" -AccountPassword (ConvertTo-SecureString -AsPlainText "PleaseChange123!") -Enabled $True
+New-ADUser -Name "Giovani Abuaitah" -SamAccountName "giovaniabuaitah" -AccountPassword (ConvertTo-SecureString -AsPlainText "PleaseChange123!") -Enabled $True
