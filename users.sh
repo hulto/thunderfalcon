f59cfaf6f1e526a3d2af51a3039323fb9c42e1f4
ProgrammingPickle <cjrezendes@yahoo.com>
2021-03-06 10:16:28 -0500

    Update users.sh

--- users.sh
+++ users.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 
-echo "bruh123469!" | passwd --stdin jimmu
-echo "bruh123469!" | passwd --stdin suizei
-echo "bruh123469!" | passwd --stdin annei
-echo "bruh123469!" | passwd --stdin itoku
-echo "bruh123469!" | passwd --stdin kinmei
-echo "bruh123469!" | passwd --stdin bidatsu
-echo "bruh123469!" | passwd --stdin yomei
-echo "bruh123469!" | passwd --stdin sushun
-echo "bruh123469!" | passwd --stdin suiko
-echo "bruh123469!" | passwd --stdin jomei
-echo "bruh123469!" | passwd --stdin kogyoku
-echo "bruh123469!" | passwd --stdin kotoku
-echo "bruh123469!" | passwd --stdin saimei
-echo "bruh123469!" | passwd --stdin tenji
-echo "bruh123469!" | passwd --stdin kobun
-echo "bruh123469!" | passwd --stdin jito
-echo "bruh123469!" | passwd --stdin monmu
-echo "bruh123469!" | passwd --stdin geme
+echo -e "bruh123469!" | passwd jimmu
+echo -e "bruh123469!" | passwd suizei
+echo -e "bruh123469!" | passwd annei
+echo -e "bruh123469!" | passwd itoku
+echo -e "bruh123469!" | passwd kinmei
+echo -e "bruh123469!" | passwd bidatsu
+echo -e "bruh123469!" | passwd yomei
+echo -e "bruh123469!" | passwd sushun
+echo -e "bruh123469!" | passwd suiko
+echo -e "bruh123469!" | passwd jomei
+echo -e "bruh123469!" | passwd kogyoku
+echo -e "bruh123469!" | passwd kotoku
+echo -e "bruh123469!" | passwd saimei
+echo -e "bruh123469!" | passwd tenji
+echo -e "bruh123469!" | passwd kobun
+echo -e "bruh123469!" | passwd jito
+echo -e "bruh123469!" | passwd monmu
+echo -e "bruh123469!" | passwd geme
