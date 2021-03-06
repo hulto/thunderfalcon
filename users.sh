b48f67e0255d081f9fd97f0292d8d8908d762e1c
ProgrammingPickle <cjrezendes@yahoo.com>
2021-03-06 11:22:43 -0500

    Update users.sh

--- users.sh
+++ users.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 echo -e "bruh123469\nbruh123469" | sudo passwd jimmu
-echo -e "bruh123469\nbruh123469" | sudo passwd suizei
-echo -e "bruh123469\nbruh123469" | sudo passwd annei
-echo -e "bruh123469\nbruh123469" | sudo passwd itoku
-echo -e "bruh123469\nbruh123469" | sudo passwd kinmei
-echo -e "bruh123469\nbruh123469" | sudo passwd bidatsu
-echo -e "bruh123469\nbruh123469" | sudo passwd yomei
-echo -e "bruh123469\nbruh123469" | sudo passwd sushun
-echo -e "bruh123469\nbruh123469" | sudo passwd suiko
-echo -e "bruh123469\nbruh123469" | sudo passwd jomei
-echo -e "bruh123469\nbruh123469" | sudo passwd kogyoku
-echo -e "bruh123469\nbruh123469" | sudo passwd kotoku
-echo -e "bruh123469\nbruh123469" | sudo passwd saimei
-echo -e "bruh123469\nbruh123469" | sudo passwd tenji
-echo -e "bruh123469\nbruh123469" | sudo passwd kobun
-echo -e "bruh123469\nbruh123469" | sudo passwd jito
-echo -e "bruh123469\nbruh123469" | sudo passwd monmu
-echo -e "bruh123469\nbruh123469" | sudo passwd geme
+echo -e "nachopassword1\nnachopassword1" | sudo passwd suizei
+echo -e "thissucks123\nthissucks123" | sudo passwd annei
+echo -e "wackbro69\nwackbro69" | sudo passwd itoku
+echo -e "whateverbruh4\nwhateverbruh4" | sudo passwd kinmei
+echo -e "cybercool2\ncybercool2" | sudo passwd bidatsu
+echo -e "nonyabiz3\nnonyabiz3" | sudo passwd yomei
+echo -e "juju567\njuju567" | sudo passwd sushun
+echo -e "ndjkh789\nndjkh789" | sudo passwd suiko
+echo -e "yodonthackme4\nyodonthackme4" | sudo passwd jomei
+echo -e "yodonthackme4\nyohackme4" | sudo passwd kogyoku
+echo -e "wearenotokay1\nwearenotokay1" | sudo passwd kotoku
+echo -e "wearenotokay1\nwearenotokay1" | sudo passwd saimei
+echo -e "helpme123\nhelpme123" | sudo passwd tenji
+echo -e "toomanypass\ntoomanypass" | sudo passwd kobun
+echo -e "toomanypass\ntoomanypass" | sudo passwd jito
+echo -e "iamnotokay69\niamnotokay69" | sudo passwd monmu
+echo -e "plzletuswin6969\nplzletuswin6969" | sudo passwd geme
