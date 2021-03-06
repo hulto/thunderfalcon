9cc69a082339d29e6cfc1ce7e2e1b2aed7501fe9
ProgrammingPickle <cjrezendes@yahoo.com>
2021-03-06 11:25:25 -0500

    Update users.sh

--- users.sh
+++ users.sh
@@ -2,19 +2,19 @@
 
 echo -e "bruh123469\nbruh123469" | sudo passwd jimmu
 echo -e "nachopassword1\nnachopassword1" | sudo passwd suizei
-echo -e "thissucks123\nthissucks123" | sudo passwd annei
+echo -e "makeastrongwarrior14\nmakeastrongwarrior14" | sudo passwd annei
 echo -e "wackbro69\nwackbro69" | sudo passwd itoku
 echo -e "whateverbruh4\nwhateverbruh4" | sudo passwd kinmei
 echo -e "cybercool2\ncybercool2" | sudo passwd bidatsu
 echo -e "nonyabiz3\nnonyabiz3" | sudo passwd yomei
-echo -e "juju567\njuju567" | sudo passwd sushun
-echo -e "ndjkh789\nndjkh789" | sudo passwd suiko
+echo -e "japaneseculturemyguy\njapaneseculturemyguy" | sudo passwd sushun
+echo -e "ifyouwantowinjustwinmyguy\nifyouwanttowinjustwinmyguy" | sudo passwd suiko
 echo -e "yodonthackme4\nyodonthackme4" | sudo passwd jomei
-echo -e "yodonthackme4\nyohackme4" | sudo passwd kogyoku
+echo -e "yodonthackme4\nyodonthackme4" | sudo passwd kogyoku
 echo -e "wearenotokay1\nwearenotokay1" | sudo passwd kotoku
 echo -e "wearenotokay1\nwearenotokay1" | sudo passwd saimei
-echo -e "helpme123\nhelpme123" | sudo passwd tenji
-echo -e "toomanypass\ntoomanypass" | sudo passwd kobun
-echo -e "toomanypass\ntoomanypass" | sudo passwd jito
+echo -e "samuraiwarriorsforlyfe\nsamuraiwarriorsforlyfe" | sudo passwd tenji
+echo -e "toomanypassnotenoughculture\ntoomanypassnotenoughculture" | sudo passwd kobun
+echo -e "toomanypassnotenoughculture\ntoomanypassnotenoughculture" | sudo passwd jito
 echo -e "iamnotokay69\niamnotokay69" | sudo passwd monmu
 echo -e "plzletuswin6969\nplzletuswin6969" | sudo passwd geme
